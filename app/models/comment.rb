898d3b94f65dea48092dda0a274f5eaa68125886
Michael Fahim <michael_fahim@yahoo.com>
2020-09-18 01:46:48 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,12 @@
+
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user
 
+  def user_attributes=(user_attributes)
+    unless user_attributes[:username].blank?
+      self.user = User.find_or_create_by(username: user_attributes[:username])
+    end
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,13 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories
 
+  def categories_attributes=(ca)
+    ca.values.each do |c|
+      next if c['name'].empty?
 
+      categories << Category.find_or_create_by(c)
+    end
+  end
 end
